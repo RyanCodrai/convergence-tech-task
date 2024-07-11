3d630a7e1d9f3966db85cb8e1354e1c1b17de2f9
Ryan Codrai <ryan.codrai@builder.ai>
2024-07-12 00:00:49 +0100

    Fix loading from .env file

--- src/settings.py
+++ src/settings.py
@@ -2,9 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
 
     OPENAI_API_KEY: str
+    LANGFUSE_SECRET_KEY: str
+    LANGFUSE_PUBLIC_KEY: str
 
 
 settings = Settings()
